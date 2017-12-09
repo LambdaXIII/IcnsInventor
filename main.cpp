9ed535ec3ba9f997fb0020864493aad3a418a19c
xiii_1991 <xiii_1991@163.com>
2017-12-09 16:55:19 +0800

    gogo

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <QCoreApplication>
+#include <QCommandLineParser>
+
+#define TRANSLATE(T) QCoreApplication::translate("main",T)
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication a(argc, argv);
+  QCoreApplication::setApplicationName("IcnsInventor");
+  QCoreApplication::setApplicationVersion("0.1alpha");
+
+  QCommandLineParser parser;
+  parser.setApplicationDescription(TRANSLATE("将一个图片文件转换成苹果icns图标的工具。"));
+  parser.addHelpOption();
+  parser.addPositionalArgument("source", TRANSLATE("原始图片文件"));
+  parser.addPositionalArgument("desitiny", TRANSLATE("需要导出的目标位置"));
+
+
+  parser.process(a);
+
+  const QStringList args = parser.positionalArguments();
+  if (args.size() < 2)
+    return 2;
+
+  const QString source_src = args[0];
+  const QString dest_src = args[1];
+
+//  return a.exec();
+  return 0;
+}
